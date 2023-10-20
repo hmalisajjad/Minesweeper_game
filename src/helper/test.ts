dfe9372a941e03daf718a00cba7be5b2ca3d20bc
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2023-10-20 16:54:53 +0200

    type_declaration

--- src/helper/test.ts
+++ src/helper/test.ts
@@ -1,3 +1,31 @@
 let a: number;
 let b: boolean;
 let c: string;
+
+interface BasicUser {
+    name: string;
+    surname: string;
+    age: number;
+    isAdmin: boolean;
+    permissions?: string[]; 
+}
+
+const user: BasicUser = {
+    name: 'Ali',
+    surname: 'Sajjad',
+    age: 31,
+    isAdmin: true,
+};
+
+const user2: BasicUser = {
+    name: 'Bilal',
+    surname: 'Ikram',
+    age: 31,
+    isAdmin: false,
+    permissions:['manager', 'operator', 'instructor'],
+};
+
+type MathFunc = (a: number, b: number) => number;
+
+const mul: MathFunc = (a, b) => a * b;
+const add: MathFunc = (a, b) => a + b;
